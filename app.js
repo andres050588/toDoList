71036dfc041f7f23c043640e308f86105a8f672f
Andres B <andres050588@mail.ru>
2021-02-28 16:29:09 +0100

    Changed the forEach loop instead the For loop in list.ejs, included mongoDB with mongoose on root route

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 const date = require(__dirname + '/date.js');
 const app = express();
-
-const toDoList = ['Buy Food', 'Cook Food', 'Eat Food'];
-const workList = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/todolistDB', {useNewUrlParser: true, useUnifiedTopology: true});
+const itemsSchema = {
+    toDoItemName: String
+}
+const Item = mongoose.model('Item', itemsSchema)
+
+const item1 = new Item({
+    toDoItemName: 'Finish 2 models section'
+})
+const item2 = new Item({
+    toDoItemName: 'Checkout if it is ok'
+})
+const item3 = new Item({
+    toDoItemName: 'Try another methodds'
+})
+const defaultItems = [item1, item2, item3];
+Item.insertMany(defaultItems, function(err){
+    if (err){
+        console.log(err);
+    }else{
+        console.log('Successfully saved to DB');
+    }
+})
+
 app.get('/', function(req, res){
-    const day = date.getDate();
-    res.render('list', {listTitle: day, newListItems: toDoList})
+    Item.find({}, function(err, foundItems){
+        res.render('list', {listTitle: 'Today', newListItems: foundItems})
+    })
 })
 
 app.post('/', function(req, res){
@@ -23,7 +46,7 @@ app.post('/', function(req, res){
         workList.push(newItem);
         res.redirect('/work')
     }else{
-        toDoList.push(newItem);
+        defaultItems.push(newItem);
         res.redirect('/');
     }
 
